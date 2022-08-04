5811314542852249c2344460e4cdf5ebe14e769c
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-08-04 17:33:51 +0200

    Testing Apollo client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_PRODUCTS = gql`
+query {
+  category(input: {title: "all"}) {
+	    products {
+      id
+      name
+      inStock
+      category
+    }
+  }
+}
+`;
+
+function DisplayProducts() {
+  const { loading, error, data } = useQuery(GET_PRODUCTS);
+  data ? console.log(data.category.products) : console.log('nothing');
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+
+  return data.category.products.map(({ id, name, inStock, category }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <br />
+      <b>In stock:</b>
+      <p>{inStock ? "Yes" : "No"}</p>
+      <br />
+      <b>Category:</b>
+      <p>{category}</p>
+    </div>
+  ));
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <br />
+      <DisplayProducts />
     </div>
   );
 }
